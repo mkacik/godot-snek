c624710979bbc51eee47e4640e3fe15b1c091118
mkacik <m.m.kacik@gmail.com>
2025-01-04 15:08:41 -0600

    Move snek head every second based on last direction input

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,12 +3,14 @@ extends Node2D
 const Common = preload("res://lib/common.gd")
 
 @export var cell_size: int = 32
-@export var speed: int = 400
+@export var speed: int = 200
 
 var max_pos: Vector2
 var min_pos: Vector2
 
 var direction: Vector2
+var next_direction: Vector2
+
 var target_position: Vector2
 
 # Called when the node enters the scene tree for the first time.
@@ -20,7 +22,8 @@ func _ready():
     var max_y = (floori(screen_size.y / cell_size) - 1) * cell_size
     max_pos = Vector2(max_x, max_y)
 
-    direction = Vector2.ZERO
+    direction = Vector2.RIGHT
+    next_direction = direction
     target_position = position
     
     $AnimatedSprite2D.animation = "idle"
@@ -28,43 +31,29 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float):
-    # if moving is in progress, animate 
-    
-    
-    # allow movement only if player was not already moving
-    if direction == Vector2.ZERO:    
-        if Input.is_action_pressed("move_right"):
-            direction.x += 1
-            $AnimatedSprite2D.animation = "right"
-        if Input.is_action_pressed("move_left"):
-            direction.x -= 1
-            $AnimatedSprite2D.animation = "left"
-        if Input.is_action_pressed("move_down"):
-            direction.y += 1
-            $AnimatedSprite2D.animation = "down"
-        if Input.is_action_pressed("move_up"):
-            direction.y -= 1
-            $AnimatedSprite2D.animation = "up"
+    if Input.is_action_pressed("move_right"):
+        next_direction = Vector2.RIGHT
+        $AnimatedSprite2D.animation = "right"
+    elif Input.is_action_pressed("move_left"):
+        next_direction = Vector2.LEFT
+        $AnimatedSprite2D.animation = "left"
+    elif Input.is_action_pressed("move_down"):
+        next_direction = Vector2.DOWN
+        $AnimatedSprite2D.animation = "down"
+    elif Input.is_action_pressed("move_up"):
+        next_direction = Vector2.UP
+        $AnimatedSprite2D.animation = "up"
 
-        if direction.length() != 0:
-            # enforce target position within screen bounds
-            target_position = Common.clamped(position + direction * cell_size, min_pos, max_pos);           
-            # if target position was not a valid one, we hit something, so it's game over
-            if position == target_position:
-                $AnimatedSprite2D.animation = "angry"
-        else:
-            $AnimatedSprite2D.animation = "idle"
-            
-    
     if position != target_position:
         var previous_position = position
         var velocity = direction * speed
-        
         # enforce new position to be between current and target position
         position = Common.clamped(position + velocity * delta, previous_position, target_position)
-    else:
-        direction = Vector2.ZERO
         
 func _on_move_timer_timeout() -> void:
-    # if new direction was selected, use it; if not, use previously selected one
-    pass
+    direction = next_direction
+    # enforce target position within screen bounds
+    target_position = Common.clamped(position + direction * cell_size, min_pos, max_pos);
+    # if target position was not changed, we hit something, so it's game over
+    if position == target_position:
+        $AnimatedSprite2D.animation = "angry"
