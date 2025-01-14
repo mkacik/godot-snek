0e4e3980c21008b52b765535e31db167c51d404f
mkacik <m.m.kacik@gmail.com>
2025-01-13 21:37:21 -0600

    Wrap unoccupied cell tracking in it's own class

--- scenes/level.gd
+++ scenes/level.gd
@@ -2,10 +2,11 @@ extends Node2D
 
 @export var cell_size: int = 16
 var grid_size: Vector2
-var unoccupied_cells = {}
+var cells: Cells
 
 func _ready() -> void:
     grid_size = get_viewport_rect().size / cell_size
+    cells = Cells.new(grid_size)
 
 func _process(_delta: float) -> void:
     pass
@@ -25,22 +26,47 @@ func position_walls() -> void:
     $WallE.apply_scale(outer_wall_y_scale)
     $WallW.apply_scale(outer_wall_y_scale)
 
-func reset_unoccupied_cells() -> void:
-    unoccupied_cells = {}
-    for x in range(1, grid_size.x - 1):
-        for y in range(1, grid_size.y - 1):
-            mark_grid_cell_unoccupied(Vector2(x, y))
-
 func start() -> void:
+    cells.initialize()
     position_walls()
-    reset_unoccupied_cells()
     show()
 
-func mark_grid_cell_occupied(grid_position: Vector2) -> void:
-    unoccupied_cells.erase(grid_position)
+func mark_grid_cell_occupied(cell: Vector2) -> void:
+    cells.mark_occupied(cell)
 
-func mark_grid_cell_unoccupied(grid_position: Vector2) -> void:
-    unoccupied_cells[grid_position] = null
+func mark_grid_cell_unoccupied(cell: Vector2) -> void:
+    cells.mark_free(cell)
     
 func get_random_unoccupied_grid_cell() -> Vector2:
-    return unoccupied_cells.keys().pick_random()
+    return cells.get_random_free_cell()
+
+
+# This class is used to keep track of free cells to spawn the apples on. When
+# picking position at random, there is a risk of selecting cell that snake is
+# currently on (or a wall with more complex levels). The longer the snake is,
+# the higher risk of not being able to find a free cell randomly in reasonable
+# time, so I chose to track the free cells instead, marking them occupied
+# when snake enters, and marking them free when tail end leaves.
+class Cells extends Node:
+    # GDScript does not have sets, but hashmap with null values will do
+    var free_cells = {}
+    var grid_size
+
+    func _init(new_grid_size: Vector2) -> void:
+        grid_size = new_grid_size
+
+    func initialize():
+        free_cells.clear()
+        for x in range(0, grid_size.x):
+            for y in range(0, grid_size.y):
+                var cell = Vector2(x, y)
+                free_cells[cell] = null
+
+    func mark_free(cell: Vector2) -> void:
+        free_cells[cell] = null
+
+    func mark_occupied(cell: Vector2) -> void:
+        free_cells.erase(cell)
+
+    func get_random_free_cell() -> Vector2:
+        return free_cells.keys().pick_random()
